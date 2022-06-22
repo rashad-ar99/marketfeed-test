e58b37a49a1854c15b22b0ac5b03751522ef8fe9
rashad-ar99 <rashad.abdurahman99@gmail.com>
2022-06-22 09:39:12 +0530

    Version 1.0

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath));
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}!`);
+ });
+
+ app.listen(port, () => {
+    console.log('Server is up!');
+ });
